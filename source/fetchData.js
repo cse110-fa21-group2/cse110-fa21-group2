b4a6b079ee263a3ff6e1f9c2a484475d95cc43a5
Faris Ashai <farisashai@gmail.com>
2021-11-03 23:52:49 -0700

    started home page outline

---
+++ source/fetchData.js
@@ -0,0 +1,12 @@
+// https://spoonacular.com/food-api/docs
+
+const getRecipes = () => {
+
+}
+
+const getBookmarks = () => {
+  const bookmarks = localStorage.getItem('bookmarks')
+
+}
+
+getBookmarks()
