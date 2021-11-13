d1fc20310e73af9b85230283b4081e700d701ec2
Kaiwen Tsou <kktsou@ucsd.edu>
2021-11-12 18:08:50 -0800

    Updated unit test skeletons

---
+++ __tests__/unit_tests.test.js
@@ -0,0 +1,6 @@
+//Simple Jest unit tests
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(1 + 2).toBe(3);
+  });
+
---
+++ __tests__/unit_tests_hard.test.js
@@ -0,0 +1,11 @@
+//We can split the files and the script should 
+//automatically run all tests
+
+test('adds 1 + 1 + 1 to equal 3', () => {
+    expect(1 + 1 + 1).toBe(3);
+  });
+
+test('adds some numbers to equal 3', () => {
+  expect(1 + 5 + 1).toBe(7);
+});
+
