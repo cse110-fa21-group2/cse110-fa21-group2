ed35cb50102d3a6f7553bab5428d6dcb1096101e
Faris Ashai <farisashai@gmail.com>
2021-11-16 22:05:08 -0800

    recipe card web component

--- source/components/RecipeCard.js
+++ source/components/RecipeCard.js
@@ -1,59 +1,150 @@
-// RecipeCard.js
-
 class RecipeCard extends HTMLElement {
   constructor() {
-    super(); // inherets everything from HTMLElement
-    this.attachShadow({ mode: 'open' }); // Creates the Shadow DOM
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.saved = false;
   }
 
   set data(data) {
-    this.json = data; // Store the data passed in for later
+    this.json = data;
 
-    // Store the element styles in a <style> block, needed bc of the shadow DOM
-    const styles = document.createElement('style');
+    const styleElem = document.createElement('style');
+    const styles = `
+    @import url("https://use.fontawesome.com/releases/v5.15.4/css/all.css");
+      .recipe-card {
+        background: white;
+        border: 1px solid black;
+        display: inline-block;
+        height: 17rem;
+        margin: 0 1.5rem 1.5rem 0;
+        width: 15rem;
+        white-space: normal;
+        overflow-y: scroll;
+        overflow-wrap: break-word;
+        border-radius: 0.5rem;
+      }
 
-    // TODO: Input style
-    styles.innerHTML = `
-      .card {
-        align-items: center;
-        background-color: white;
-        border-radius: 3px;
-        display: grid;
-        grid-template-areas:
-        'image'
-        'title'
-        'star';
-        height: 200px;
-        margin: auto;
-        padding: 2px, 1px;
-        width: 400px;
+      .card-shadow {
+        border: none;
+        box-shadow: 0 2px 5px rgba(0,0,0,0.6);
+        transition: 0.2s ease-in-out;
+      }
+      .card-shadow:hover, .card-shadow:focus {
+        border: none;
+        transform: scale(1.01);
+        box-shadow: 0 8px 20px rgba(0,0,0,0.6);
+      }
+
+      .card-header {
+        font-size: 1.5rem;
+        margin-bottom: 0.5rem;
+      }
+
+      .card-header.card-img {
+        overflow: hidden;
+      }
+      .card-header.card-img > img{
+        display: block;
+        width: 100%;
+        aspect-ratio: 16/9;
+        object-fit: cover;
+      }
+
+      .card-body {
+        font-size: 0.8rem;
+        padding: 0 1rem;
+      }
+
+      .card-footer {
+        padding: 0 1rem 1rem 1rem;
+      }
+
+      .card-btn {
+        background: var(--color);
+        color: white;
+        border: none;
+        --color: hsl(200, 50%, 50%);
+        cursor: pointer;
+        transition: 0.2s ease-in-out;
+        border-radius: 0.25em;
+        padding: 0.5em 0.75em;
+      }
+
+      .card-btn:hover, .card-btn:focus {
+        background: hsl(200, 50%, 60%);
+      }
+
+      .card-btn.card-btn-outline {
+        background: none;
+        border: 1px solid var(--color);
+        color: rgb(255, 52, 52);
+      }
+
+      .card-btn.card-btn-outline:hover, .card-btn.card-btn-outline:focus {
+        /* background: hsl(200, 50%, 90%); */
       }
     `;
+    styleElem.innerHTML = styles;
+    this.shadowRoot.append(styleElem);
 
-    // Create the outer wrapper for the recipe card to nest inside
-    const wrapper = document.createElement('div');
-    wrapper.classList.add('card');
+    const card = document.createElement('article');
+    card.classList.add('recipe-card');
+    card.classList.add('card-shadow');
 
-    // Create the recipe image element
-    const img = document.createElement('img');
-    img.setAttribute('src', data.image);
-    img.setAttribute('alt', data.title);
-    img.setAttribute('width', 200);
+    const header = document.createElement('div');
+    header.classList.add('card-header');
+    header.classList.add('card-img');
 
-    // Create the recipe title
-    const title = document.createElement('p');
-    title.classList.add('title');
-    title.innerHTML = data.title;
+    const recipePic = document.createElement('img');
+    recipePic.src = 'https://source.unsplash.com/random';
 
-    // Assigning "this" to variable to access inside func
-    const recipeCard = this;
+    header.appendChild(recipePic);
+    card.appendChild(header);
 
+    const body = document.createElement('div');
+    body.classList.add('card-body');
 
-    // Add all of the above elements to the wrapper
-    wrapper.append(img, title);
+    const title = document.createElement('h1');
+    title.innerHTML = 'Recipe Name';
 
-    // Append the wrapper and the styles to the Shadow DOM
-    this.shadowRoot.append(styles, wrapper);
+    body.appendChild(title);
+    card.appendChild(body);
+
+    const footer = document.createElement('div');
+    footer.classList.add('card-footer');
+
+    const viewRecipe = document.createElement('button');
+    viewRecipe.classList.add('card-btn');
+    viewRecipe.innerHTML = 'View Recipe';
+
+    const saveRecipe = document.createElement('button');
+    saveRecipe.classList.add('card-btn');
+    saveRecipe.classList.add('card-btn-outline');
+
+    const saveIcon = document.createElement('i');
+    saveIcon.classList.add('far');
+    saveIcon.classList.add('fa-heart');
+
+    const flipSaved = (element) => {
+      if (this.saved) {
+        element.children[0].classList.add('far');
+        element.children[0].classList.remove('fas');
+      } else {
+        element.children[0].classList.remove('far');
+        element.children[0].classList.add('fas');
+      }
+      this.saved = !this.saved;
+    };
+
+    saveRecipe.addEventListener('onclick', flipSaved);
+    saveRecipe.appendChild(saveIcon);
+
+    footer.appendChild(viewRecipe);
+    footer.appendChild(saveRecipe);
+
+    card.appendChild(footer);
+
+    this.shadowRoot.append(card);
   }
 
   get data() {
