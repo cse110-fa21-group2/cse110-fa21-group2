28921038fcde028de06f8a6adf498671536205fe
mreswick <mreswick@ucsd.edu>
2021-11-19 17:46:28 -0800

    some recipe data particular fetchers added; in progress

--- source/scripts/recipePageElement.js
+++ source/scripts/recipePageElement.js
@@ -257,7 +257,7 @@ class RecipeExpand extends HTMLElement {
 
         <div class="title-edit">
           <p class = "title"> </p>
-          <button> Edit </buttoin>
+          <button> Edit </button>
         </div>
 
         <!-- Rating Info: -->
@@ -468,6 +468,10 @@ class RecipeExpand extends HTMLElement {
           </main>
         </section>
       `;
+
+      // Assume we're passed a single recipe as data
+
+
   
       // Set Title
       const title = getTitle(data).toUpperCase();
@@ -540,248 +544,163 @@ class RecipeExpand extends HTMLElement {
       return this.json;
     }
   }
-  
-  /**
-   * Recursively search for a key nested somewhere inside an object
-   * @param {Object} object the object with which you'd like to search
-   * @param {String} key the key that you are looking for in the object
-   * @returns {*} the value of the found key
-   */
-  function searchForKey(object, key) {
-    var value;
-    Object.keys(object).some(function (k) {
+  customElements.define('recipe-expand', RecipeExpand);
+
+ // from lab 7:
+function searchForKey(object, key) {
+  var value;
+  Object.keys(object).some(function (k) {
       if (k === key) {
-        value = object[k];
-        return true;
+      value = object[k];
+      return true;
       }
       if (object[k] && typeof object[k] === 'object') {
-        value = searchForKey(object[k], key);
-        return value !== undefined;
+      value = searchForKey(object[k], key);
+      return value !== undefined;
       }
-    });
-    return value;
+  });
+  return value;
   }
   
+  function getTitle(data) {
+    return searchForKey(data, "title");
+  }
+
+  function getRecID(data) {
+    return searchForKey(data, "id");
+  }
+
+  // gets img for recipe
+  function getImgSrc(data) {
+    return searchForKey(data, "image");
+  }
+  
+  // returns array of ingredients
+  function getIngredArray(data) {
+    // an array of ingredient objects:
+    let ingredList = searchForKey(data, "extendedIngredients");
+    ingredListLen = ingredList.length;
+    let ingredListArr = []
+    // populate ingredients list (append each time):
+    for(let i = 0; i < ingredListLen; i++) {
+      ingredListArr[i] = ingredList[i]("name");
+    }
+    return ingredListArr;
+  }
+
+  // returns ingredients as single
+  // comma-separated string:
+  function getIngredString(data) {
+    // an array of ingredient objects:
+    let ingredList = searchForKey(data, "extendedIngredients");
+    ingredListLen = ingredList.length;
+    let ingredListStr = "";
+    // populate ingredients list (append each time):
+    for(let i = 0; i < ingredListLen; i++) {
+      ingredListString += ingredList[i]("name");
+      // add comma after all but last ingredient:
+      if(i != ingredListLen - 1) {
+        ingredListString += ", ";
+      }
+    }
+    return ingredListString;
+  }
+
+  // returns array of U.S. measurements:
+  function getUSMeasurArray(data) {
+    // an array of ingredient objects:
+    let ingredList = searchForKey(data, "extendedIngredients");
+    ingredListLen = ingredList.length;
+    let measurementsArr = "";
+    // populate measurements list (append each time):
+    for(let i = 0; i < ingredListLen; i++) {
+      measurementsArr[i]  
+        = ingredList[i]["measures"]["us"]["amount"]
+          + ingredList[i]["measures"]["us"]["unitShort"];
+      
+    }
+    return measurementsArr;
+  }
+
+  // returns string of U.S. measurements:
+  function getUSMeasurArray(data) {
+    // an array of ingredient objects:
+    let ingredList = searchForKey(data, "extendedIngredients");
+    ingredListLen = ingredList.length;
+    let measurementsStr = "";
+    // populate measurements list (append each time):
+    for(let i = 0; i < ingredListLen; i++) {
+      measurementsStr
+        += ingredList[i]["measures"]["us"]["amount"]
+          + ingredList[i]["measures"]["us"]["unitShort"];
+      // add comma after all but last measurement:
+      if(i != ingredListLen - 1) {
+        measurementsStr += ", ";
+      }
+    }
+  }
+
+  // returns rating (**NOTE: NO RATING GIVEN, SO REPLACE THIS;
+  // DUMMY SPOONACULAR SCORE USED FOR NOW**)
+  function getRatingStat(data){
+    return searchForKey(data, "spoonacularScore");
+  }
+
+  // returns total number of likes:
+  function getNumLikes(data){
+    return searchForKey(data, "aggregateLikes");
+  }
+
+  // returns recipe short description / summary:
+  function getSummary(data) {
+    return searchForKey("summary");
+  }
+
+  
+
+  function getUSMeasures(data) {
+    return searchForKey(data, )
+  }
+
+
+
+
+
   /**
-   * Extract the title of the recipe from the given recipe schema JSON obejct
-   * @param {Object} data Raw recipe JSON to find the image of
-   * @returns {String} If found, returns the recipe title
+   * 
+   * @param {Object} data // assume data is passed as object for single recipe 
    */
   function getTitle(data) {
-    if (data.name) return data.name;
-    if (data['@graph']) {
-      for (let i = 0; i < data['@graph'].length; i++) {
-        if (data['@graph'][i]['@type'] == 'Recipe') {
-          if (data['@graph'][i]['name']) return data['@graph'][i]['name'];
-        };
-      }
-    }
-    return null;
+      return searchForKey(data, "originalName");
   }
   
-  /**
-   * Extract the yield of the recipe from the given recipe schema JSON obejct
-   * @param {Object} data Raw recipe JSON to find the image of
-   * @returns {String} If found, returns the recipe yield
-   */
-  function getYield(data) {
-    if (data.recipeYield) return data.recipeYield;
-    if (data['@graph']) {
-      for (let i = 0; i < data['@graph'].length; i++) {
-        if (data['@graph'][i]['@type'] == 'Recipe') {
-          if (data['@graph'][i]['recipeYield']) {
-            if (Array.isArray(data['@graph'][i]['recipeYield'])) {
-              return data['@graph'][i]['recipeYield'][0];
-            } else if (typeof data['@graph'][i]['recipeYield'] == 'string') {
-              return data['@graph'][i]['recipeYield'];
-            }
-          }
-        }
-      }
-    }
-    return null;
+  // returns measurements in US units:
+  function getUSMeasures(data) {
+      return searchForKey(data["measures"], "us");
   }
   
-  /**
-   * Extract the categories of the recipe from the given recipe schema JSON obejct
-   * @param {Object} data Raw recipe JSON to find the image of
-   * @returns {String} If found, returns the recipe categories as a string
-   */
-  function getCategories(data) {
-    let categories = null;
-    if (data.recipeCategory) {
-      categories = data.recipeCategory
-    } else if (data['@graph']) {
-      for (let i = 0; i < data['@graph'].length; i++) {
-        if (data['@graph'][i]['@type'] == 'Recipe') {
-          if (data['@graph'][i]['recipeCategory']) {
-            categories = data['@graph'][i]['recipeCategory'];
-          }
-        };
-      }
-    }
-    if (Array.isArray(categories)) categories = categories.join(', ');
-    return categories.toLowerCase();
+  // get recipe's ID:
+  function getRecID(data) {
+      return searchForKey(data, "id");
   }
   
-  /**
-   * Extract the description of the recipe from the given recipe schema JSON obejct
-   * @param {Object} data Raw recipe JSON to find the image of
-   * @returns {String} If found, returns the recipe description
-   */
-  function getDescription(data) {
-    if (data.description) return data.description;
-    if (data['@graph']) {
-      for (let i = 0; i < data['@graph'].length; i++) {
-        if (data['@graph'][i]['@type'] == 'Recipe') {
-          return data['@graph'][i]['description'];
-        }
-      }
-    }
-    return null;
+  // get recipe's ingredients:
+  function getIngred(data) {
+      // TODO: get individual step ingredients (in nested .JSON objects)
+      return searchForKey(data, "instructions");
   }
   
-  /**
-   * Extract a usable image from the given recipe schema JSON object
-   * @param {Object} data Raw recipe JSON to find the image of
-   * @returns {String} If found, returns the URL of the image as a string, otherwise null
-   */
-  function getImage(data) {
-    if (data.image?.url) return data.image.url;
-    if (data.image?.contentUrl) return data.image.contentUrl;
-    if (data.image?.thumbnail) return data.image.thumbnail;
-    if (data['@graph']) {
-      for (let i = 0; i < data['@graph'].length; i++) {
-        if (data['@graph'][i]['@type'] == 'ImageObject') {
-          if (data['@graph'][i]['url']) return data['@graph'][i]['url'];
-          if (data['@graph'][i]['contentUrl']) return data['@graph'][i]['contentUrl'];
-          if (data['@graph'][i]['thumbnailUrl']) return data['@graph'][i]['thumbnailUrl'];
-        };
-      }
-    }
-    return null;
+  // get img for recipe
+  function getImg(data) {
+      return searchForKey(data, "image");
   }
   
-  /**
-   * Extract the URL from the given recipe schema JSON object
-   * @param {Object} data Raw recipe JSON to find the URL of
-   * @returns {String} If found, it returns the URL as a string, otherwise null
-   */
-  function getUrl(data) {
-    if (data.url) return data.url;
-    if (data['@graph']) {
-      for (let i = 0; i < data['@graph'].length; i++) {
-        if (data['@graph'][i]['@type'] == 'Recipe') return data['@graph'][i]['@id'];
-      }
-    };
-    return null;
+  // try to get quick facts (slightly modified from Design Team's specs
+  // as couldn't find all the info):
+  function getQuickFacts(data) {
+      return searchForKey(data, "nameClean") + "\n" 
+      + searchForKey(data, consistency) + "\n"
+      + searchForKey(data, "unit") + "\n"
+      + searchForKey(data, "unit"); 
   }
-  
-  /**
-   * Similar to getUrl(), this function extracts the organizations name from the
-   * schema JSON object. It's not in a standard location so this function helps.
-   * @param {Object} data Raw recipe JSON to find the org string of
-   * @returns {String} If found, it retuns the name of the org as a string, otherwise null
-   */
-  function getOrganization(data) {
-    if (data.publisher?.name) return data.publisher?.name;
-    if (data['@graph']) {
-      for (let i = 0; i < data['@graph'].length; i++) {
-        if (data['@graph'][i]['@type'] == 'WebSite') {
-          return data['@graph'][i].name;
-        }
-      }
-    };
-    return null;
-  }
-  
-  /**
-   * Converts ISO 8061 time strings to regular english time strings.
-   * Not perfect but it works for this lab
-   * @param {String} time time string to format
-   * @return {String} formatted time string
-   */
-  function convertTime(time) {
-    let timeStr = '';
-  
-    // Remove the 'PT'
-    time = time.slice(2);
-  
-    let timeArr = time.split('');
-    if (time.includes('H')) {
-      for (let i = 0; i < timeArr.length; i++) {
-        if (timeArr[i] == 'H') return `${timeStr} hr`;
-        timeStr += timeArr[i];
-      }
-    } else {
-      for (let i = 0; i < timeArr.length; i++) {
-        if (timeArr[i] == 'M') return `${timeStr} min`;
-        timeStr += timeArr[i];
-      }
-    }
-  
-    return '';
-  }
-  
-  /**
-   * Extract the ingredients of the recipe from the given recipe schema JSON obejct
-   * @param {Object} data Raw recipe JSON to find the image of
-   * @returns {Array} If found, returns the recipe ingredients
-   */
-  function getIngredients(data) {
-    if (data.recipeIngredient) {
-      if (typeof data.recipeIngredient == 'string') {
-        return data.recipeIngredient.slit('. ');
-      }
-      return data.recipeIngredient;
-    };
-    if (data['@graph']) {
-      for (let i = 0; i < data['@graph'].length; i++) {
-        if (data['@graph'][i]['@type'] == 'Recipe') {
-          if (typeof data['@graph'][i]['recipeIngredient'] == 'string') {
-            return data['@graph'][i]['recipeIngredient'].slit('. ');
-          }
-          return data['@graph'][i]['recipeIngredient'];
-        };
-      }
-    }
-    return null;
-  }
-  
-  /**
-   * Extract the instructions of the recipe from the given recipe schema JSON obejct.
-   * This ones a bit messy and optimally should be refactored but it works.
-   * @param {Object} data Raw recipe JSON to find the image of
-   * @returns {Array} If found, returns the recipe instructions
-   */
-  function getInstructions(data) {
-    if (data.recipeInstructions) {
-      if (typeof data.recipeInstructions == 'string') {
-        return data.recipeInstructions.split('. ');
-      }
-      return data.recipeInstructions;
-    };
-    if (data['@graph']) {
-      for (let i = 0; i < data['@graph'].length; i++) {
-        if (data['@graph'][i]['@type'] == 'Recipe') {
-          if (data['@graph'][i]['recipeInstructions'] == 'string') {
-            return data['@graph'][i]['recipeInstructions'].split('. ');
-          }
-          if (data['@graph'][i]['recipeInstructions'][0]['itemListElement']) {
-            const instructionArr = [];
-            data['@graph'][i]['recipeInstructions'].forEach(instrObj => {
-              instrObj.itemListElement.forEach(instruction => {
-                instructionArr.push(instruction.text);
-              });
-            });
-            return instructionArr;
-          } else {
-            return data['@graph'][i]['recipeInstructions'].map(instr => instr.text);
-          }
-        };
-      }
-    }
-    return null;
-  }
-  
-  customElements.define('recipe-expand', RecipeExpand);
+
