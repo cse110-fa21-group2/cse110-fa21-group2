bb86bc3a3466030bfe71a0d61f7896774c30247f
mreswick <mreswick@ucsd.edu>
2021-11-21 12:47:15 -0800

    updated files (deleted unnecessary); router working; expanded page working (needs some extra implementation like for save button, etc.; also no nutrition facts); return button on expanded page implemented (as browser back arrow)

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -18,66 +18,6 @@ let savedRecipePage;
 let searchResults;
 let recipeInfoPage;
 
-
-// let fetchedData;
-
-/*
-async function init() {
-  // TODO
-  // Load 10 recipes per category for explore page (offset=rand to get diff recipe everytime)
-  // Populate explore page cards
-  // add recipes to localstorage
-  // navigate to explore page
-
-  // fetch saved recipe cards from localstorage
-  // navigate to saved recipe page
-  // add save button action to every card and page to update the saved recipe page so we don't
-  // have to handle it when we open the page
-
-  // add event handler to search button to fetch recipes for query
-  // add recipes to localstorage
-  // populate search result cards
-  // navigate to search results page
-
-  landingPage = document.querySelector('.landing');
-  explorePage = document.querySelector('.explore');
-  savedRecipePage = document.querySelector('.saved-recipes');
-  searchResults = document.querySelector('.search-results');
-  recipeInfoPage = document.querySelector('.recipe-info');
-
-  
-  router = new Router(() => {
-    landingPage.classList.remove('hidden');
-    explorePage.classList.add('hidden');
-    savedRecipePage.classList.add('hidden');
-    searchResults.classList.add('hidden');
-    recipeInfoPage.classList.add('hidden');
-  });
-  
-
-  // Function calls
-  // Fetch recipes and store locally if storage is empty.
-  let storageCategoryData = fetcherFuncs.getAllCategoryRecipe();
-  const categoryNames = ['popular', 'cheap', 'healthy', 'fast'];
-  for (let i = 0; i < categoryNames.length; i++) {
-    if (!(categoryNames[i] in storageCategoryData)
-        || storageCategoryData[categoryNames[i]].length < 10) {
-        // fetch from api for (categoryNames[i], 10)
-       const fetchedData = [];
-       // store to local storage
-       storageFuncs.storeRecipeData(categoryNames[i], fetchedData);
-    }
-  }
-  storageCategoryData = fetcherFuncs.getAllCategoryRecipe();
-  createExploreRecipeCards(storageCategoryData);
-  
-  const storageSavedData = fetcherFuncs.getAllSavedRecipe();
-}
-*/
-// window.addEventListener('DOMContentLoaded', init);
-
-
-
 router = new Router(() => {
 	landingPage.classList.remove('hidden');
 	//explorePage.classList.add('hidden');
