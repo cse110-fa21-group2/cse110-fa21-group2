2e0e8c37de5432b835d0c013788cbc328f88cb91
treeebooor <robbertjiang@gmail.com>
2021-11-21 19:21:40 -0800

    comments

--- source/js/apiHelpers.js
+++ source/js/apiHelpers.js
@@ -8,7 +8,8 @@ import { getAllRecipes, getSingleRecipe } from './storage/fetcher.js';
 // require('dotenv').config();
 
 // const fetch = require('node-fetch');// uncomment if using with nodejs
-const { API_KEY } = process.env;// prevent exposing api key
+// const { API_KEY } = process.env;// prevent exposing api key
+// const API_KEY = 'blahblahblah'
 
 const HOST = 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com';
 
