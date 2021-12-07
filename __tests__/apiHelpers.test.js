01e0acbd5ffb322b403ab620bdcbc1951f14ee58
treeebooor <robbertjiang@gmail.com>
2021-12-07 11:53:50 -0800

    get some tests to work

--- __tests__/apiHelpers.test.js
+++ __tests__/apiHelpers.test.js
@@ -1,12 +1,13 @@
-import * as helpers from '../source/scripts/apiHelpers.js'; // import doesnt work 
+import * as helpers from '../source/scripts/apiHelpers.js';
+import storeRecipeData from '../source/scripts/storage/storage.js';
 
-// havent acutally tried running anything yet(idk if they work at all)
+// set mock local storage so api functions wont break
+localStorage.setItem('recipeData', '{}');
 
 // getDetailedRecipeInfoBulk
 test('check getDetailedRecipeInfoBulk with empty list input', () => helpers.getDetailedRecipeInfoBulk([]).then((data) => {
-  expect(data).toBe([]);
+  expect(data).toEqual([]);
 }));
-
 test('check getDetailedRecipeInfoBulk with ids', () => helpers.getDetailedRecipeInfoBulk([547775, 547899]).then((data) => {
   expect(data.length).toBe(2);
 }));
@@ -16,21 +17,22 @@ test('Check getRecipesByName with filtering options', () => {
   const filter = {};
   filter.sort = 'popularity';
   filter.sortDirection = 'desc';
-  filter.diet = 'vegitarian';
+  filter.diet = 'vegetarian';
   filter.cuisine = 'mexican';
   return helpers.getRecipesByName('rice', 1, 0, filter).then((data) => {
     const recipe = data[0];
+    console.log(recipe.vegetarian, recipe.cuisines, recipe.veryPopular);
     expect(recipe.vegetarian).toBe(true);
-    expect('Mexican' in recipe.cuisines).toBe(true);
+    expect(recipe.cuisines.includes('Mexican')).toBe(true);
     expect(recipe.veryPopular).toBe(true);
   });
 });
 
 // getRecipesByCuisine
 test('Check getRecipesByCuisine Mexican returns mexican dishes', () => helpers.getRecipesByCuisine('mexican', 1).then((data) => {
-  expect('Mexican' in data[0].cuisines).toBe(true);
+  expect(data[0].cuisines.includes('Mexican')).toBe(true);
 }));
 // getRecipesByType
 test('Check getRecipesByType breakfast returns breakfast dishes', () => helpers.getRecipesByType('breakfast', 1).then((data) => {
-  expect('breakfast' in data[0].dishTypes).toBe(true);
+  expect(data[0].dishTypes.includes('breakfast')).toBe(true);
 }));
