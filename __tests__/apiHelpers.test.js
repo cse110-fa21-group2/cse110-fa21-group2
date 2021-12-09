50bed511fdedd4d0d3a7dcc6ee35b2ebc7e00f07
Harry_Yang <hay015@ucsd.edu>
2021-12-08 22:40:43 -0800

    add test case

--- __tests__/apiHelpers.test.js
+++ __tests__/apiHelpers.test.js
@@ -20,6 +20,15 @@ test('check getDetailedRecipeInfoBulk with empty list input', () => helpers.getD
 test('check getDetailedRecipeInfoBulk with ids', () => helpers.getDetailedRecipeInfoBulk([547775, 547899]).then((data) => {
   expect(data.length).toBe(2);
 }));
+test('check getDetailedRecipeInfoBulk with ids', () => helpers.getDetailedRecipeInfoBulk([547775, 547899]).then((data) => {
+  expect(data.length).toBe(2);
+}));
+test('check getDetailedRecipeInfoBulk with ids', () => helpers.getDetailedRecipeInfoBulk([123456, 654321, 145682, 423784]).then((data) => {
+  expect(data.length).toBe(4);
+}));
+test('check getDetailedRecipeInfoBulk with ids', () => helpers.getDetailedRecipeInfoBulk([123456, 654321, 145682, 423784, 54894, 54955]).then((data) => {
+  expect(data.length).toBe(6);
+}));
 
 // getRecipesByName filtering
 test('Check getRecipesByName with filtering options', () => {
@@ -36,6 +45,34 @@ test('Check getRecipesByName with filtering options', () => {
   });
 });
 
+test('Check getRecipesByName with filtering options', () => {
+  const filter = {};
+  filter.sort = 'popularity';
+  filter.sortDirection = 'asc';
+  filter.diet = 'Vegan';
+  filter.cuisine = 'Italian';
+  return helpers.getRecipesByName('pizza', 1, 0, filter).then((data) => {
+    const recipe = data[0];
+    expect(recipe.Vegan).toBe(true);
+    expect(recipe.cuisines.includes('Italian')).toBe(true);
+    expect(recipe.veryPopular).toBe(true);
+  });
+});
+
+test('Check getRecipesByName with filtering options', () => {
+  const filter = {};
+  filter.sort = 'popularity';
+  filter.sortDirection = 'asc';
+  filter.diet = 'Vegan';
+  filter.cuisine = 'Italian';
+  return helpers.getRecipesByName('Pasta', 1, 0, filter).then((data) => {
+    const recipe = data[0];
+    expect(recipe.Vegan).toBe(true);
+    expect(recipe.cuisines.includes('Italian')).toBe(true);
+    expect(recipe.veryPopular).toBe(true);
+  });
+});
+
 // extractIDs
 test('Check that extractIDs is checking for local storage', () => {
   const dummyRecipeData = { 776505: 'awoogabooga' };
@@ -47,11 +84,39 @@ test('Check that extractIDs is checking for local storage', () => {
   expect(ids[1]).toEqual(['awoogabooga']);
 });
 
+test('Check that extractIDs is checking for local storage', () => {
+  const dummyRecipeData = { 658451: 'mydummy1' };
+  setRecipeData(dummyRecipeData);
+  //console.log(getAllRecipes);
+  const dummyComplexSearch = { results: [{ id: 658451 }, { id: 6548 }] };
+  const ids = helpers.extractIDs(dummyComplexSearch);
+  expect(ids[0]).toEqual([6548]);
+  expect(ids[1]).toEqual(['mydummy1']);
+});
+
+test('Check that extractIDs is checking for local storage', () => {
+  const dummyRecipeData = { 111111: '111111' };
+  setRecipeData(dummyRecipeData);
+  //console.log(getAllRecipes);
+  const dummyComplexSearch = { results: [{ id: 53465 }, { id: 111111 }, { id: 2222 }] };
+  const ids = helpers.extractIDs(dummyComplexSearch);
+  expect(ids[0]).toEqual([2222]);
+  expect(ids[1]).toEqual(['111111']);
+  expect(ids[2]).toEqual([53465]);
+});
+
 // getRecipesByCuisine
 test('Check getRecipesByCuisine Mexican returns mexican dishes', () => helpers.getRecipesByCuisine('mexican', 1).then((data) => {
   expect(data[0].cuisines.includes('Mexican')).toBe(true);
 }));
+test('Check getRecipesByCuisine Mexican returns mexican dishes', () => helpers.getRecipesByCuisine('mexican', 1).then((data) => {
+  expect(data[0].cuisines.includes('pizza')).toBe(false);
+}));
+
 // getRecipesByType
 test('Check getRecipesByType breakfast returns breakfast dishes', () => helpers.getRecipesByType('breakfast', 1).then((data) => {
   expect(data[0].dishTypes.includes('breakfast')).toBe(true);
+}));
+test('Check getRecipesByType breakfast returns breakfast dishes', () => helpers.getRecipesByType('breakfast', 1).then((data) => {
+  expect(data[0].dishTypes.includes('lunch')).toBe(false);
 }));
